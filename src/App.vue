415a94324bc2438cc4a084cca7ab1474610bd015
Alisson Andrade <alisson14101998@gmail.com>
2022-01-06 14:25:38 -0300

    Validation and delete event

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
   <div id="app">
+    <h3>Cadastro clientola: </h3>
+    <small v-show="erro" id="nomeErro">Nome inválido!</small><br>
+    <input type="text" placeholder="Nome" v-model="nomeField"><br>
+    <input type="text" placeholder="E-mail" v-model="emailField"><br>
+    <input type="number" placeholder="Idade" v-model="idadeField">
+    <button @click="registerCustomer">Cadastrar</button>
+    <hr>
     <div v-for="(cliente,index) in clientes" :key="cliente.id">
       <h3>{{index+1}}</h3>
-      <Cliente :cliente="cliente" :showAge="true"/>
-      <hr>
+      <Cliente :cliente="cliente" :showAge="true" @deleteMe="deleteCustomer($event)"/>
       <h4>Edição: </h4>
       <input type="text" v-model="cliente.nome">
       <input type="text" v-model="cliente.email">
+      <hr>
     </div>
     
   </div>
@@ -20,6 +27,10 @@ export default {
   name: 'App',
   data(){
     return {
+      erro: false,
+      nomeField: "",
+      emailField: "",
+      idadeField: 0,
       clientes: [
         {
           id: 1,
@@ -46,10 +57,35 @@ export default {
   components: {
     Cliente,
     //Produto
+  },
+  methods: {
+    registerCustomer: function(){
+      if(this.nomeField == "" || this.nomeField == " " || this.nomeField.length < 3){
+        this.erro = true;
+      }else{
+        this.clientes.push({
+        id: Object.keys(this.clientes).length + 1,
+        nome: this.nomeField,
+        email: this.emailField,
+        idade: this.emailField
+      })
+
+      this.nomeField = "";
+      this.emailField = "";
+      this.idadeField = 0;
+      this.erro = false;
+      }
+    },
+    deleteCustomer: function($event){
+      var newArray = this.clientes.filter(cliente => cliente.id != $event.idCliente);
+      this.clientes = newArray;
+    }
   }
 }
 </script>
 
 <style>
-
+  #nomeErro{
+    color:red;
+  }
 </style>
--- src/components/Cliente.vue
+++ src/components/Cliente.vue
@@ -5,6 +5,8 @@
         <p>E-mail: {{cliente.email}}</p>
         <p v-if="showAge">Idade: {{cliente.idade}}</p>
         <p v-else>O usuário é corno e não quer exibir sua idade.</p>
+        <button @click="changeClienteClass($event)">Mudar</button>
+        <button @click="deleteClientEvent($event)">Deletar</button>
     </div>
 </template>
 
@@ -18,6 +20,15 @@ export default {
     props: {
         cliente: Object,
         showAge: Boolean
+    },
+    methods: {
+        changeClienteClass: function($event){
+            console.log($event);
+            this.isPremium = !this.isPremium;
+        },
+        deleteClientEvent: function(){
+            this.$emit("deleteMe", {idCliente: this.cliente.id, component: this});
+        }
     }
 }
 
@@ -29,7 +40,7 @@ export default {
         background-color: darkgrey;
         margin: 10px;
         width: 500px;
-        height: 140px;
+        height: 180px;
     }
 
     .cliente-premium{
@@ -38,7 +49,7 @@ export default {
         color: gold;
         margin: 10px;
         width: 500px;
-        height: 140px;
+        height: 180px;
 
     }
 </style>
